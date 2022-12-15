5a416d77e982cc9d443e7178c98fe1534d9f98d0
LeeeeSuHyeon <tngus0673@naver.com>
2022-12-15 18:00:29 +0900

    Initial Commit

---
+++ Bamin_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bamin_tutorial
+//
+//  Created by 이수현 on 2022/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
